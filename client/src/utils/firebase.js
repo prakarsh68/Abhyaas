ecb6086f6f6ed7d43d9a9e1fb5ebc76cbfafbce1
MAN-123-JIT <pkkaushik2006@gmail.com>
2026-07-09 23:20:25 +0530

    Slight changes in names

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,11 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-ba6ba.firebaseapp.com",
-  projectId: "interviewiq-ba6ba",
-  storageBucket: "interviewiq-ba6ba.firebasestorage.app",
-  messagingSenderId: "862159592601",
-  appId: "1:862159592601:web:7308d702cd708076ddec08"
+  authDomain: "abhyaas-cd8dc.firebaseapp.com",
+  projectId: "abhyaas-cd8dc",
+  storageBucket: "abhyaas-cd8dc.firebasestorage.app",
+  messagingSenderId: "838047015655",
+  appId: "1:838047015655:web:6d67615790505d97473842"
 };
 
 const app = initializeApp(firebaseConfig);
